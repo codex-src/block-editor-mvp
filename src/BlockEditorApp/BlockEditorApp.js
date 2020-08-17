fecaf86e588ee3ad6718a2d41108a91e97244008
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-17 23:35:02 +0900

    Reduced the search bar size

--- src/BlockEditorApp/BlockEditorApp.js
+++ src/BlockEditorApp/BlockEditorApp.js
@@ -105,11 +105,11 @@ const BlockEditorApp = () => {
 
 					{/* Search bar */}
 					<div className="relative">
-						<div className="absolute inset-y-0 left-0 pointer-events-none">
+						<div className="px-4 absolute inset-y-0 left-0 pointer-events-none">
 							<div className="flex flex-row items-center h-full">
 								<ApplyTransition>
-									<Apply className="mx-4 text-gray-400 transform scale-90" style={{ color: searchText && "var(--gray-800)" }}>
-										<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="search w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+									<Apply className="text-gray-400" style={{ color: searchText && "var(--gray-800)" }}>
+										<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="search w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
 									</Apply>
 								</ApplyTransition>
 							</div>
@@ -118,8 +118,8 @@ const BlockEditorApp = () => {
 							<Apply className="bg-gray-100 focus:outline-none">
 								<input
 									id="search-bar"
-									className="px-6 w-full h-12 text-lg text-gray-800 rounded-full"
-									style={{ paddingLeft: tw(4 + 6 + 2) }}
+									className="px-4 w-full h-10 !text-lg text-gray-800 rounded-full"
+									style={{ paddingLeft: tw(4 + 5 + 2) }}
 									type="text"
 									placeholder="Search"
 									onChange={e => setSearchText(e.target.value)}
