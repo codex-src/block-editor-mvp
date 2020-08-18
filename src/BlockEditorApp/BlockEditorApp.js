ce57ca54c21f679981c76f51bfb1b616369a0b5a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-18 17:50:25 +0900

    Shifting to new UI

--- src/BlockEditorApp/BlockEditorApp.js
+++ src/BlockEditorApp/BlockEditorApp.js
@@ -78,20 +78,20 @@ const BlockEditorApp = () => {
 	// const [state, dispatch] = useImmerReducer(BlockEditorReducer, initialState)
 
 	const searchInputRef = React.useRef(null)
-	const searchHeaderRef = React.useRef(null)
+	// const searchHeaderRef = React.useRef(null)
 
 	const [scrollYBeforeSearchText, setScrollYBeforeSearchText] = React.useState(0)
 	const [searchText, setSearchText] = React.useState("")
 
-	// Effect for simulating flex flex-row items-baseline.
-	React.useLayoutEffect(() => {
-		if (!searchText) {
-			// No-op
-			return
-		}
-		const nudge = (searchHeaderRef.current.offsetHeight - searchInputRef.current.offsetHeight) / 2
-		searchHeaderRef.current.style.marginTop = (-searchInputRef.current.offsetHeight + -nudge) + "px"
-	}, [searchText])
+	// // Effect for simulating flex flex-row items-baseline.
+	// React.useLayoutEffect(() => {
+	// 	if (!searchText) {
+	// 		// No-op
+	// 		return
+	// 	}
+	// 	const nudge = (searchHeaderRef.current.offsetHeight - searchInputRef.current.offsetHeight) / 2
+	// 	searchHeaderRef.current.style.marginTop = (-searchInputRef.current.offsetHeight + -nudge) + "px"
+	// }, [searchText])
 
 	// Effect for auto-scrolling on searchText.
 	React.useEffect(() => {
@@ -113,98 +113,33 @@ const BlockEditorApp = () => {
 
 	// NOTE: Uses items-start because of sticky top-0.
 	return (
-		<div className="px-4 sm:px-6 py-48 flex flex-row justify-center items-start">
+		<div className="px-4 sm:px-6 py-32 flex flex-row justify-center items-start">
 
 			{/* LHS */}
-			<Apply className="-mt-48 sticky top-0" style={{ paddingTop: tw(48 - 12 /* Search bar */ - 8 /* h-8 */) }}>
+			<Apply className="-mt-32 pt-32 sticky top-0">
 				<aside className="flex-shrink-0 hidden md:block w-64">
 
-					{/* Search bar */}
-					<div className="relative">
-
-						{/* Search bar LHS */}
-						{!searchText && (
-							<div className="px-6 absolute inset-y-0 left-0 pointer-events-none">
-								<div className="flex flex-row items-center h-full">
-									<ApplyTransition>
-										<Apply className="text-gray-300">
-											<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="search w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-											{/* <svg viewBox="0 0 20 20" fill="currentColor" className="filter w-5 h-5"><path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" /></svg> */}
-										</Apply>
-									</ApplyTransition>
-								</div>
-							</div>
-						)}
-
-						{/* <input type="text"> */}
-						<ApplyTransition>
-							{/* <Apply className={!searchText ? "bg-gray-100" : "bg-white border border-gray-200"}> */}
-							<Apply className={!searchText ? "bg-gray-100" : "bg-white shadow-hero"}>
-								<input
-									id="search-bar"
-									ref={searchInputRef}
-									className="px-6 w-full h-12 text-gray-800 rounded-full focus:outline-none"
-									style={{
-										paddingLeft: !searchText && tw(6 + 5 + 2), //  tw(4 /* px-4 */ + 5 /* w-5 h-5 */ + 2),
-										paddingRight: tw(4 /* px-4 */ + 1 /* p-1 */ + 4 /* w-4 h-4 */ + 1 /* p-1 */ + 2),
-										fontSize: tw(4.25),
-										// borderRadius: tw(3.5),
-									}}
-									type="text"
-									placeholder="Search"
-									value={searchText}
-									onChange={e => setSearchText(e.target.value)}
-									{...disableAutoCorrect}
-								/>
-							</Apply>
-						</ApplyTransition>
-
-						{/* Search bar RHS */}
-						{/* {searchText && ( */}
-						<Transition
-							on={searchText}
-							className="transition duration-200 ease-in-out"
-							from="opacity-0 transform scale-0 pointer-events-none"
-							to="opacity-100 transform scale-100 pointer-events-auto"
-						>
-							<div className="px-4 absolute inset-y-0 right-0">
-								<div className="flex flex-row items-center h-full">
-									<button
-										// NOTE: Uses text-teal-500 not text-teal-400.
-										className="-mr-1 p-1 bg-transparent text-teal-500 bg-teal-50 rounded-full focus:outline-none"
-										onClick={e => {
-											searchInputRef.current.focus()
-											setSearchText("")
-										}}
-										aria-label="Clear search"
-									>
-										<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="x w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M6 18L18 6M6 6l12 12" /></svg>
-									</button>
-								</div>
-							</div>
-						</Transition>
-						{/* )} */}
-
-					</div>
-
 					{/* Contents */}
-					<div className="h-8" />
-					<ul className="space-y-3">
+					<ul className="space-y-2">
 
 						{/* Header */}
 						<li>
 							<Apply className="flex flex-row items-center">
 								<p className="font-medium text-xs tracking-wider truncate text-gray-400">
-									<Apply className="mr-1.5 transform scale-90">
-										<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="menu-alt2 w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M4 6h16M4 12h16M4 18h7" /></svg>
+									<Apply className="mr-1.5 transform scale-90 origin-left">
+										<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="menu-alt2 w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h7" /></svg>
+										{/* <svg viewBox="0 0 20 20" fill="currentColor" className="menu-alt2 w-4 h-4"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg> */}
 									</Apply>
+									{/* TABLE OF CONTENTS */}
 									{"Adele – Hello".toUpperCase()}
 								</p>
 							</Apply>
 						</li>
 
 						{/* Subheaders */}
-						<li>
+						{/* */}
+						{/* NOTE: Uses pt-* because of space-y-*. */}
+						<li className="pt-2">
 							<p
 								className="text-sm truncate text-gray-500"
 								style={{
@@ -314,26 +249,24 @@ const BlockEditorApp = () => {
 			<main className="w-full max-w-2xl">
 
 				{searchText && (
-					<div className="relative">
-						<div className="-mt-8 absolute top-0 inset-x-0">
-							<DocumentTitle title={`Searching “${searchText}”`}>
-								<h1 ref={searchHeaderRef} className="font-bold text-4xl truncate text-gray-800">
-									Searching{" "}
-									{/* <span className="text-gray-300"> */}
-									“
-									{/* </span> */}
-									{/* <span className="text-white bg-teal-400 rounded"> */}
-									<span className="underline" style={{ textDecorationColor: "var(--teal-400)" }}>
-										{searchText.trim()}
-									</span>
-									{/* <span className="text-gray-300"> */}
-									”
-									{/* </span> */}
-								</h1>
-							</DocumentTitle>
-						</div>
-					</div>
+					<DocumentTitle title={`Searching “${searchText}”`} />
 				)}
+
+				{/* {searchText && ( */}
+				{/* 	<div className="relative"> */}
+				{/* 		<div className="-mt-8 absolute top-0 inset-x-0"> */}
+				{/* 			<DocumentTitle title={`Searching “${searchText}”`}> */}
+				{/* 				<h1 ref={searchHeaderRef} className="font-bold text-4xl truncate text-gray-800"> */}
+				{/* 					Searching “ */}
+				{/* 					<span className="underline" style={{ textDecorationColor: "var(--teal-400)" }}> */}
+				{/* 						{searchText.trim()} */}
+				{/* 					</span> */}
+				{/* 					” */}
+				{/* 				</h1> */}
+				{/* 			</DocumentTitle> */}
+				{/* 		</div> */}
+				{/* 	</div> */}
+				{/* )} */}
 
 				<FakeContent />
 			</main>
